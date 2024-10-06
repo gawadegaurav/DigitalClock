52aedaebc6672ad905abb3b588d1b580f769a707
gawadegaurav <gawadegaurav2005@gmail.com>
2024-10-06 20:00:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+setInterval(()=>{
+
+    let hrs=document.getElementById("hrs");
+    let min=document.getElementById("min");
+    let sec=document.getElementById("sec");
+    
+
+    let currentTime = new Date();
+
+hrs.innerHTML=currentTime.getHours();
+min.innerHTML=currentTime.getMinutes();
+sec.innerHTML=currentTime.getSeconds();
+
+
+},1000)
+
+console.log("hi")
